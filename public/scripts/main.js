0d6d11c15971d59fc106ef795465010923575c2f
Mikko Koski <mikko.koski@futurice.com>
2013-08-12 11:28:54 +0300

    jQuery

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,17 @@
-define(function() {
-	"use strict";
-	console.log("This is required file");
+requirejs.config({
+    paths: {
+        jquery: [
+            '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
+            '../lib/jquery' // your fallback
+        ],
+    },
+});
+
+define(['jquery'], function($) {
+    "use strict";
+    console.log("This is required file");
+
+    if($) {
+        console.log("jQuery is loaded!");
+    }
 });
