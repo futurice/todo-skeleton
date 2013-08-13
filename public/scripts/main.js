c8e9fa3e63614ef352a180736804c6d538419647
Mikko Koski <mikko.koski@futurice.com>
2013-08-13 15:24:50 +0300

    Add RequireJS text plugin and examples how to use _.template

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,27 @@
+/**
+    To load text dependencies, use define like this:
+
+    define(['jquery', 'underscore', 'text!partials/hello.html'], 
+        function($, _, hello) { .. }
+
+    To use text template:
+
+    var helloTemplate = _.template(hello);
+    var helloHtml = helloTemplate({name: "Mikko"});
+    var helloElement = $(helloHtml);
+    $("body").prepend(helloElement);
+    
+*/
+
 requirejs.config({
     paths: {
         jquery: [
             '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
             '../lib/jquery' // your fallback
         ],
-        underscore: '../lib/underscore'
+        underscore: '../lib/underscore',
+        text: '../lib/text',
+        partials: '../partials'
     },
     shim: {
         'underscore': {
